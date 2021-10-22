2f819908a586c8e2b65c44a68621f2c82acefec1
Gavin Murphy <gavinhmurphy@gmail.com>
2021-10-21 17:37:14 -0700

    working in workbench

--- db/schema.sql
+++ db/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE role(
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2),
     department_id INT, 
-    CONSTRAiNT fk_department FOREIGN KEY (departmant_id) REFERENCES department(id) ON DELETE CASCADE
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 
 );
 -- 
@@ -26,9 +26,9 @@ CREATE TABLE employee(
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    CONTRANT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
     manager_id INT,
-    CONSTRAiNT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 
 );
 -- two foreign keys

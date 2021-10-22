8a0068c2f28310507f0421fc2c18072cd94af669
Gavin Murphy <gavinhmurphy@gmail.com>
2021-10-21 17:32:13 -0700

    syntax fix

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+-- table contents
+DROP DATABASE IF EXISTS tracker_db;
+CREATE DATABASE tracker_db;
+USE tracker_db;
+
+CREATE TABLE department(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30) NOT NULL
+
+-- foreignkey
+);
+
+
+CREATE TABLE role(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2),
+    department_id INT, 
+    CONSTRAiNT fk_department FOREIGN KEY (departmant_id) REFERENCES department(id) ON DELETE CASCADE
+
+);
+-- 
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    CONTRANT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    manager_id INT,
+    CONSTRAiNT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+
+);
+-- two foreign keys

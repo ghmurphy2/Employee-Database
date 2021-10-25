084cb5765eb0bb0bb173c38b4d836b13f948773c
Gavin Murphy <gavinhmurphy@gmail.com>
2021-10-24 20:04:06 -0700

    palce holder async

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // each query prints to database
 // take input from inquirer
+// json data
+// No need to print html
+// use menu, see all deparment employees etc employee id 
 
 const inquirer = require('inquirer')
 const db = require('./db')
@@ -15,3 +18,32 @@ async function viewAllDepartments(){
 }
  
 viewAllDepartments()
+
+async function addEmployee(){
+  db.query('SELECT * FROM employee', function (err, results) {
+    console.log(results);
+  });
+
+  app.use((req, res) => {
+    res.status(404).end();
+  }); 
+}
+
+async function deleteEmployee(){
+    db.query(`DELETE employee WHERE id = ?`, 3, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log("affectedRows: ", result.affectedRows);
+        app.use((req, res) => {
+            res.status(404).end();
+      });
+
+
+}
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  })

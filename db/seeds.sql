f417343cf7f621de9d7e3cce4f9bdc3f28af0890
Gavin Murphy <gavinhmurphy@gmail.com>
2021-10-24 19:17:53 -0700

    base seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,33 +3,21 @@ use tracker_db;
 INSERT INTO department
     (name)
 VALUES
-    ('Targaryen'),
-    ('Lannister'),
-    ('Stark');
+    ('Sales'),
+    ('Programming'),
+    ('Human Reources');
 
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('Targaryen Queen', 750000, 1),
-    ('Targaryen King', 500000, 1),
-    ('Targaryen Knight', 50000, 1),
-    ('Lannister King', 1000000, 2),
-    ('Lannister Queen', 1000000, 2),
-    ('Lannister Knight', 60000, 2),
-    ('Stark King', 250000, 3),
-    ('Stark Queen', 250000, 3),
-    ('Stark Knight', 50000, 3);
+    ('Sales Manager', 750000, 1),
+    ('Front End Engineer', 500000, 1),
+    ('Sales Intern', 50000, 1),
+    ('Programming Manager', 1000000, 2),
+    ('Back End Engineer', 1000000, 2),
+    ('Programming Intern', 60000, 2),
+    ('HR Manager', 250000, 3),
+    ('HR Representative', 250000, 3),
+    ('HR Inter', 50000, 3);
 
 
-INSERT INTO employee 
-    (first_name, last_name, role_id, manager_id)
-VALUES  
-    ('Daenerys', 'Targaryen', 1, NULL),
-    ('Rhaehgar', 'Targaryen', 2, 1),
-    ('George', 'Targaryen', 3, 1),
-    ('Tywin', 'Lannister', 4, NULL),
-    ('Joanna', 'Lannister', 5, 4),
-    ('Jamie', 'Lannister', 6, 4),
-    ('Ed', 'Stark', 7, NULL),
-    ('Sansa', 'Stark', 8, 7),
-    ('Jon', 'Snow', 9, 7);
